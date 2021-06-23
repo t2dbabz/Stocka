425883fa17cac3c612dd34591e9e43b1b809a95f
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-06-23 07:34:15 +0100

    Implement navigation from account setup to Home screen

--- app/src/main/java/com/example/pjt114/stocka/MainActivity.kt
+++ app/src/main/java/com/example/pjt114/stocka/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pjt114.stocka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -23,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         // Setup the bottom navigation view with navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigationView.setupWithNavController(navController)
+
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.viewPagerFragment
+                || destination.id == R.id.splashScreenFragment
+                || destination.id == R.id.accountSetUpFragment
+                || destination.id == R.id.signUpFragment
+                || destination.id == R.id.loginFragment) {
+
+                bottomNavigationView.visibility = View.GONE
+            } else {
+
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
 
     }
 }
