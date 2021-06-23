419f18fd94e2f7aaff5745a60b9d2d0a22543a56
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-06-23 17:00:39 +0100

    Add Circle indicator to the Onboarding screen

--- app/src/main/java/com/example/pjt114/stocka/ui/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/example/pjt114/stocka/ui/onboarding/ViewPagerFragment.kt
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.pjt114.stocka.R
 import com.example.pjt114.stocka.ui.onboarding.screens.*
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class ViewPagerFragment : Fragment() {
@@ -39,7 +41,21 @@ class ViewPagerFragment : Fragment() {
 
         view.findViewById<ViewPager2>(R.id.view_pager).adapter = adapter
 
+
+
+
         return  view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       val tabLayout = view.findViewById<TabLayout>(R.id.into_tab_layout)
+        val viewPager2 = view.findViewById<ViewPager2>(R.id.view_pager)
+
+        TabLayoutMediator(tabLayout, viewPager2) {
+            tab, position ->
+        }.attach()
     }
 
 
